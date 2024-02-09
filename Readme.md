06d446cf60b043aaa2d4513ec28efcae92f52404
jagadeeeshaby <jagadeeshaby@yahoo.com>
2024-02-09 13:09:16 -0800

    Login flows

---
+++ Readme.md
@@ -0,0 +1,20 @@
+### Getting started 
+
+```
+npm install
+npm run start
+```
+
+### For certs 
+
+install homebrew 
+
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+
+and then run the below commands 
+
+```
+brew install mkcert
+mkcert -install
+mkcert localhost
+```
---
+++ server.js
@@ -0,0 +1,22 @@
+// Import builtin NodeJS modules to instantiate the service
+const https = require("https");
+const fs = require("fs");
+const helmet = require("helmet");
+
+
+// Import the express module
+const express = require("express");
+
+// Instantiate an Express application
+const app = express();
+.
+https
+    .createServer({
+        key: fs.readFileSync("cert/localhost-key.pem"),
+        cert: fs.readFileSync("cert/localhost.pem"),
+    }, app)
+    .listen(5000, () => {
+        console.log('server is runing at port 5000')
+    });
+
+app.use(express.static('public'));
